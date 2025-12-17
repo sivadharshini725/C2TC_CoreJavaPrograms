8120973b53b316273cb33380268cc097899e5d64
Sivadharshini <Sivadharshini@LAPTOP-QH34T23B>
2025-12-17 09:13:42 +0530

    commit 3

---
+++ Java_Code/src/com/tnsif/basics/IfElseDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.basics;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=18;
+		if(age>=18) {
+			System.out.println("you are eligible for voting");
+		}
+		else {
+			System.out.println("you are not eligible for voting");
+		}
+
+	}
+
+}
