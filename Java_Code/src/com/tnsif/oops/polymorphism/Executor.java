9df2ba3288862afc3428806cc73a131ca4f31825
Sivadharshini <Sivadharshini@LAPTOP-QH34T23B>
2025-12-18 10:25:50 +0530

    commit

---
+++ Java_Code/src/com/tnsif/oops/polymorphism/Executor.java
@@ -0,0 +1,18 @@
+package com.tnsif.oops.polymorphism;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Is 121 Palindrome? "+Operations.isPalindrome(121));
+		System.out.println("Is 123 Palindrome? "+Operations.isPalindrome(123));
+		
+		System.out.println("Is CAR Palindrome? "+Operations.isPalindrome("CAR"));
+		System.out.println("Is RACECAR Palindrome? "+Operations.isPalindrome("RACECAR"));
+
+	
+
+
+	}
+
+}
